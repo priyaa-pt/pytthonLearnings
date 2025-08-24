8fcf323f8913ccef9bd4b9f47a9be582096e108e
Priya Tiwari <pt20022004@gmail.com>
2025-08-25 00:47:50 +0530

    Initial Commit: added into.py

---
+++ intro.py
@@ -0,0 +1,37 @@
+
+#? INTRO TO PYTHON:
+
+# single line comment
+print("hello \n universe")
+
+''' this type of comments 
+are known as multiline comments ''' 
+
+# Escape sequence character
+print("hello all we are \"happy to connect\" with you")
+
+# data types 
+a = 1
+b = 1.1 
+c = "hellow"
+d = True
+print ( "the type of a is" , type(a))
+print ( "the type of b is" , type(b))
+print ( "the type of c is" , type(c))
+print ( "the type of d is" , type(d))
+
+# Lists (MUTABLE)
+
+list1 = [2, 2.2, ["yellow", 66],["apple","cherry"]]
+print(list1)
+
+#Tuples (IMMUTABLE)
+
+tuple1 = ("hie", "money",(2 , 1.3),(8*8, "cream"))
+print(tuple1)
+
+# Dictionary 
+
+dict1 = {"name" : "sophia" , "age" : 22 , "height" : 6}
+print(dict1)
+
