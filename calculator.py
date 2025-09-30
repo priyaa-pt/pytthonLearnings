8b25081f73a7a481cf26ea5df8a7a05dd6eeb18b
Priya Tiwari <pt20022004@gmail.com>
2025-09-30 19:05:06 +0530

    Added calculator.py file

---
+++ calculator.py
@@ -0,0 +1,30 @@
+
+#?CALCULATOR    
+
+n = 5
+m = 5
+
+n = int(input("Enter the first number: "))
+m = int(input("Enter the second number: "))
+
+ans1 = n+m
+print ("addition of",n,"and" ,m, "is", ans1)
+
+ans2 = n-m
+print ("subtraction of",n,"and" ,m, "is" ,  ans2)
+
+ans3 = n/m
+print ("division of",n,"and" ,m, "is" , ans3)
+
+ans4 = n*m
+print ("multiplication of",n,"and" ,m, "is" , ans4)
+
+ans5 = n%m
+print ("module of",n,"and" ,m, "is" , ans5)
+
+ans6 = n**m
+print ("exponent of",n,"and" ,m, "is" , ans6)
+
+
+
+
