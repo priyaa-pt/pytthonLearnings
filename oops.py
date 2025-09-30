1f939536122db844452d89377d2b71e0ce40cd32
Priya Tiwari <pt20022004@gmail.com>
2025-09-30 19:41:20 +0530

    Added oops.py

---
+++ oops.py
@@ -0,0 +1,17 @@
+# creating a class
+class student:
+    name = "golden swan"
+
+# creating object(instance)
+s1 = student()
+print(s1.name)
+
+class car:
+    name = "mercedes"
+    color = "black"
+car1 = car()
+print (car1.color)
+
+
+
+###? CONSTRUCTORS 
